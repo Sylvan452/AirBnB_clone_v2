d96389e03a3479bfdb331fda51da88bdec7e2c6d
Christiano112 <enyiachristian12@gmail.com>
2023-04-19 05:21:31 -0700

    cleared all the errors in the console

--- models/review.py
+++ models/review.py
@@ -11,9 +11,9 @@ from sqlalchemy.orm import relationship
 class Review(BaseModel, Base):
     """ Review class to store review information """
 
+    __tablename__ = 'reviews'
     """ check for database """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'reviews'
         text = Column(String(1024),
                 nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'),
--- models/state.py
+++ models/state.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage_type
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+from os import getenv
 
 class State(BaseModel, Base):
     """ State class / table model"""
     __tablename__ = 'states'
-    if storage_type == 'db':
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
