e0bbfb72abcac88341a0cd1ece7a8f2c079fd3d2
Christiano112 <enyiachristian12@gmail.com>
2023-04-19 18:22:43 -0700

    fixed all the errors in the console.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage_type
+from os import getenv
 from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
     '''amenity class'''
     __tablename__ = 'amenities'
-    if storage_type == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
     else:
         name = ""
--- models/review.py
+++ models/review.py
@@ -3,13 +3,13 @@
 from sqlalchemy.sql.schema import ForeignKey
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from models import storage_type
+from os import getenv
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
-    if storage_type == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
